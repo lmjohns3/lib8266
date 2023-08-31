a611cabbec2bc5f2c0a2dd83a79feb329cd3c7ad
Leif Johnson <lmjohns3@gmail.com>
2023-08-31 09:49:14 -0700

    Update GPIO helpers.

--- include/lib8266/io.h
+++ include/lib8266/io.h
@@ -5,9 +5,25 @@
 
 #include "driver/adc.h"
 #include "driver/gpio.h"
+#include "esp8266/gpio_struct.h"
 
-esp_err_t ahoy_io_init(uint8_t adc_clock_div,
-                       uint32_t input_pin_bit_mask,
-                       uint32_t output_pin_bit_mask);
+esp_err_t ahoy_io_init_adc(uint8_t clock_div);
+esp_err_t ahoy_io_init_inputs(uint32_t pin_bit_mask, bool enable_pullup);
+esp_err_t ahoy_io_init_outputs(uint32_t pin_bit_mask);
+
+// https://github.com/wdim0/esp8266_direct_gpio
+// https://www.reddit.com/r/esp32/comments/f529hf
+
+#define AHOY_GPIO_ENABLE_OUTPUT(pin) (GPIO.enable_w1ts |= BIT(pin))
+#define AHOY_GPIO_DISABLE_OUTPUT(pin) (GPIO.enable_w1tc |= BIT(pin))
+
+#define AHOY_GPIO_ENABLE_PULLUP gpio_pullup_en
+#define AHOY_GPIO_DISABLE_PULLUP gpio_pullup_dis
+
+#define AHOY_GPIO_READ(pin) ((GPIO.in >> pin) & 1)
+
+#define AHOY_GPIO_WRITE(pin, value) ((value) ? AHOY_GPIO_WRITE_H(pin) : AHOY_GPIO_WRITE_L(pin))
+#define AHOY_GPIO_WRITE_H(pin) (GPIO.out_w1ts |= BIT(pin))
+#define AHOY_GPIO_WRITE_L(pin) (GPIO.out_w1tc |= BIT(pin))
 
 #endif
--- lib/io.c
+++ lib/io.c
@@ -1,34 +1,39 @@
 #include "lib8266/io.h"
 
-esp_err_t ahoy_io_init(uint8_t adc_clock_div,
-                       uint32_t input_pin_bit_mask,
-                       uint32_t output_pin_bit_mask) {
-  if (adc_clock_div > 0) {
-    adc_config_t cfg;
-    cfg.mode = ADC_READ_TOUT_MODE;
-    cfg.clk_div = adc_clock_div;
-    AHOY_RETURN_IF_NOT_OK(adc_init(&cfg));
-  }
+esp_err_t ahoy_io_init_adc(uint8_t clock_div) {
+  adc_config_t cfg;
+  cfg.mode = ADC_READ_TOUT_MODE;
+  cfg.clk_div = clock_div;
+  return adc_init(&cfg);
+}
 
-  if (input_pin_bit_mask) {
-    gpio_config_t cfg;
-    cfg.pin_bit_mask = input_pin_bit_mask;
-    cfg.mode = GPIO_MODE_INPUT;
-    cfg.pull_up_en = GPIO_PULLUP_ENABLE;
-    cfg.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    cfg.intr_type = GPIO_INTR_DISABLE;
-    AHOY_RETURN_IF_NOT_OK(gpio_config(&cfg));
+esp_err_t ahoy_io_init_inputs(uint32_t pin_bit_mask, bool enable_pullup) {
+  gpio_config_t cfg;
+  cfg.pin_bit_mask = pin_bit_mask;
+  cfg.mode = GPIO_MODE_INPUT;
+  cfg.pull_up_en = enable_pullup ? GPIO_PULLUP_ENABLE : GPIO_PULLUP_DISABLE;
+  cfg.pull_down_en = GPIO_PULLDOWN_DISABLE;
+  cfg.intr_type = GPIO_INTR_DISABLE;
+  ESP_ERROR_CHECK(gpio_config(&cfg));
+  for (uint8_t i = 0; i < GPIO_NUM_MAX; ++i) {
+    if (pin_bit_mask & BIT(i)) gpio_get_level(i);
   }
+  return ESP_OK;
+}
 
-  if (output_pin_bit_mask) {
-    gpio_config_t cfg;
-    cfg.pin_bit_mask = output_pin_bit_mask;
-    cfg.mode = GPIO_MODE_OUTPUT;
-    cfg.pull_up_en = GPIO_PULLUP_DISABLE;
-    cfg.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    cfg.intr_type = GPIO_INTR_DISABLE;
-    AHOY_RETURN_IF_NOT_OK(gpio_config(&cfg));
+esp_err_t ahoy_io_init_outputs(uint32_t pin_bit_mask) {
+  gpio_config_t cfg;
+  cfg.pin_bit_mask = pin_bit_mask;
+  cfg.mode = GPIO_MODE_OUTPUT;
+  cfg.pull_up_en = GPIO_PULLUP_DISABLE;
+  cfg.pull_down_en = GPIO_PULLDOWN_DISABLE;
+  cfg.intr_type = GPIO_INTR_DISABLE;
+  ESP_ERROR_CHECK(gpio_config(&cfg));
+  for (uint8_t i = 0; i < GPIO_NUM_MAX; ++i) {
+    if (pin_bit_mask & BIT(i)) {
+      gpio_set_level(i, 1);
+      gpio_set_level(i, 0);
+    }
   }
-
   return ESP_OK;
 }
