986319ada37f20c78423713b34fbd6e9cc9db1ad
Leif Johnson <lmjohns3@gmail.com>
2023-09-15 22:38:02 -0700

    Remove unused variable.

--- lib/base.c
+++ lib/base.c
@@ -27,7 +27,7 @@ inline void ahoy_busy_wait_ticks(uint32_t ticks) {
 
 /* https://stackoverflow.com/questions/51752284/how-to-calculate-crc8-in-c */
 uint8_t ahoy_crc8(uint8_t polynomial, const uint8_t *bytes, uint8_t num_bytes) {
-  uint16_t i = 0, j = 0, crc = 0xff;
+  uint16_t i = 0, crc = 0xff;
   for (i = 0; i < num_bytes; ++i) {
     crc ^= bytes[i];
     crc = crc & 0x80 ? (crc << 1) ^ polynomial : crc << 1;
